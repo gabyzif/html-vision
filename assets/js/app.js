0fd45b7a238f21bd91ff013d87e475a0cdc3a893
gabyzif <gabyzif@gmail.com>
2018-12-17 02:12:11 -0300

    mujerbonita and general imgs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,114 @@
+var cantimgtot = -1;
+var longitud;
 $( document ).ready(function() {
 
-    $.getJSON("assets/js/hermosa.json", function(result) {
 
-      //  console.log(result);
-        json = result;
-        jsonString=JSON.stringify(json);
-        console.log(json);
+    $.getJSON("assets/js/hermosa.json", function(hermosa) {
 
-        $("#cantidad_img").append(json.length);
-
-        $.each(json, function(i, field) {
+        //  console.log(result);
+        jsonHermosa = hermosa;
+        jsonString=JSON.stringify(jsonHermosa);
+        longitud=jsonHermosa.length;
+        console.log(jsonHermosa);
 
 
-
-            var tag;
-
-            tag = "beauty";
+        $("#cantidad_img_hermosa").append(jsonHermosa.length);
 
 
-           // $("#hermosa").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
-
-
-            if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
-                $("#beauty").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
-            }
-
-            tag = "fashion model"
-            var tag_2="model"
-
-            if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag || field.tag_01 === tag_2||field.tag_02 === tag_2||field.tag_03 === tag_2||field.tag_04 === tag_2||field.tag_05 === tag_2||field.tag_06===tag_2 || field.tag_07 === tag_2 || field.tag_08 === tag_2 || field.tag_09 === tag_2 || field.tag_00  === tag_2){
-                $("#model").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
-            }
-
-
-            tag= "girl"
-
-            if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
-                $("#girl").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
-            }
-
-            tag= "woman"
-
-            if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
-                $("#woman").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
-            }
-
-
-            tag= "lady"
-
-            if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
-                $("#lady").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
-            }
+        checker(jsonHermosa,"hermosa");
 
 
 
-        });
+
+
+
+    });
+
+    $.getJSON("assets/js/mujerbonita.json", function(hermosa) {
+
+        //  console.log(result);
+        jsonMbonita = hermosa;
+        jsonString=JSON.stringify(jsonMbonita);
+        console.log(jsonMbonita);
+
+        $("#cantidad_img_mujerbonita").append(jsonMbonita.length);
+
+
+        checker(jsonMbonita,"mujerbonita");
+
+
+
 
     });
 
 
 
 
+
+
+
 });
+
+function checker(array,type){
+
+    $.each(array, function(i, field) {
+
+
+
+        var tag;
+
+        tag = "beauty";
+
+
+        // $("#hermosa").append("<img width='100px' src='assets/img/hermosa"+field.imageurl+"'>");
+
+
+        if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
+            $('#beauty-' + type).append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+            $('#beauty-general').append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+
+        }
+
+        tag = "fashion model"
+        var tag_2="model"
+
+        if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag || field.tag_01 === tag_2||field.tag_02 === tag_2||field.tag_03 === tag_2||field.tag_04 === tag_2||field.tag_05 === tag_2||field.tag_06===tag_2 || field.tag_07 === tag_2 || field.tag_08 === tag_2 || field.tag_09 === tag_2 || field.tag_00  === tag_2){
+            $('#model-' + type).append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+            $('#model-general').append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+
+        }
+
+
+        tag= "girl"
+
+        if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
+            $('#girl-' + type).append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+            $('#girl-general').append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+
+        }
+
+        tag= "woman"
+
+        if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
+            $('#woman-' + type).append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+            $('#woman-general').append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+
+        }
+
+
+        tag= "lady"
+
+        if(field.tag_01 === tag||field.tag_02 === tag||field.tag_03 === tag||field.tag_04 === tag||field.tag_05 === tag||field.tag_06===tag || field.tag_07 === tag || field.tag_08 === tag || field.tag_09 === tag || field.tag_00  === tag){
+            $('#lady-' + type).append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+            $('#lady-general').append("<img width='100px' src='assets/img/"+type+field.imageurl+"'>");
+
+        }
+
+
+
+    });
+
+    return;
+
+
+}
